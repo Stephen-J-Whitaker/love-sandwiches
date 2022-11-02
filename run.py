410a868e590445bca0f7d556b74d63283d1b2449
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2022-11-02 21:16:48 +0000

    Report stock requirements for next market

--- run.py
+++ run.py
@@ -132,8 +132,19 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
 
+
+def get_stock_values(data):
+    headings = SHEET.worksheet("stock").row_values(1)
+    stock_values = {key: data for key, data in zip(headings, data)}
+    return stock_values
+
+
+stock_values = get_stock_values(stock_data)
+print("Make the following numbers of sandwiches for next market:\n")
+print(stock_values)
